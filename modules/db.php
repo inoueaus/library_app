ab0e0f9759a77776b3f7f84a7a5a7938b10219d1
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-09-05 18:08:17 +0900

    change db settings

--- modules/db.php
+++ modules/db.php
@@ -1,11 +1,11 @@
 <?php
 
-$url = getenv('DATABASE_URL');
-$host = getenv("HOST");
-$dbname = getenv("DBNAME");
-$user = getenv("DB_USER");
-$port = getenv("DB_PORT");
-$password = getenv("PASSWORD");
+// $url = getenv('DATABASE_URL');
+// $host = getenv("HOST");
+// $dbname = getenv("DBNAME");
+// $user = getenv("DB_USER");
+// $port = getenv("DB_PORT");
+// $password = getenv("PASSWORD");
 
 //$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require options='--client_encoding=UTF8'");
 
@@ -13,7 +13,7 @@ $password = getenv("PASSWORD");
 
 //$connection = pg_connect("postgres://njtlaaftbwbafq:610605a1853cdd6a0da7eb4824584b0138b364f05a31273be057ba24a4dec9b7@ec2-50-17-255-244.compute-1.amazonaws.com:5432/d8jlr34i3eu31l");
 
-$connection = pg_connect($url);
+$connection = pg_connect(getenv('DATABASE_URL'));
 
 
 ?>
