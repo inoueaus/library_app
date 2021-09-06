0027a41af28463472cf192553f9e039bfe9763c0
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-09-06 11:33:37 +0900

    changed config back to host=

--- modules/db.php
+++ modules/db.php
@@ -20,7 +20,7 @@ $instancename = getenv("CLOUD_SQL_INSTANCE_NAME");
 
 //$connection = new PDO($dsn,$user,$password);
 
-$connection = pg_connect("socket=$instancename port=5432 dbname=$dbname user=$user password=$password options='--client_encoding=UTF8'");
+$connection = pg_connect("host=$instancename port=5432 dbname=$dbname user=$user password=$password options='--client_encoding=UTF8'");
 
 //$connection = pg_connect("host=ec2-50-17-255-244.compute-1.amazonaws.com port=5432 dbname=d8jlr34i3eu31l user=njtlaaftbwbafq password=610605a1853cdd6a0da7eb4824584b0138b364f05a31273be057ba24a4dec9b7 sslmode=require");
 
